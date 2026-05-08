5125eacf7be070b8607e3e980e28a024749addb9
Adam Skoufis <askoufis@users.noreply.github.com>
2026-05-08 12:57:52 +1000

    Set supported safari version to `>=15.4` (#79)

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ module.exports = [
   'Chrome >= 95',
   'ChromeAndroid >= 95',
   'Edge >= 95',
-  'Safari >= 15.0',
-  'iOS >= 15.0',
+  'Safari >= 15.4',
+  'iOS >= 15.4',
   'Firefox >= 98',
   'Samsung >= 16',
 ];
